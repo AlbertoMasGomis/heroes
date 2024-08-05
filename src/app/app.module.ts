48fe3e8fe6cc05a8d78f7c133598e0cf3733fd60
Alberto <amasgomis@gmail.com>
2024-08-05 17:02:06 +0200

    HeroService incluido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AuthRoutingModule } from './auth/auth-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
     AuthRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
