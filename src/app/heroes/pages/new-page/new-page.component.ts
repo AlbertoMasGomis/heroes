9b2aed6895d2256cdb83b36bda9866eb87088f6c
Alberto <amasgomis@gmail.com>
2024-08-07 08:27:16 +0200

    Finalizada sección 14

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styleUrls: ['./new-page.component.css']
+  styleUrls: ['./new-page.component.css'],
 })
 export class NewPageComponent {
-
+  public publishers = [
+    {
+      id: 'DC Commics',
+      desc: 'DC - Commics',
+    },
+    {
+      id: 'Marvel Commics',
+      desc: 'Marvel - Commics',
+    },
+  ];
 }
